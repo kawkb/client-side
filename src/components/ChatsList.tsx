bc4103893ac9afbde801b34dbc5f9eeef73e2ee2
sigma-hub <houssna555@gmail.com>
2023-03-23 23:48:47 +0000

    fixed chat list scroll

--- src/components/ChatsList.tsx
+++ src/components/ChatsList.tsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function ChatsList() {
   return (
-	<div className='chats-list scrollable'>
+	<div className='chat-list scrollable'>
+		<div className='chat-list-item'>
+			<span>Kawkab</span>
+		</div>
+		<div className='chat-list-item'>
+			<span>Radi</span>
+		</div>
+		<div className='chat-list-item'>
+			<span>Ziyad</span>
+		</div>
+		<div className='chat-list-item'>
+			<span>Sigma</span>
+		</div>
+		<div className='chat-list-item'>
+			<span>Kawkab</span>
+		</div>
+		<div className='chat-list-item'>
+			<span>Radi</span>
+		</div>
+		<div className='chat-list-item'>
+			<span>Ziyad</span>
+		</div>
+		<div className='chat-list-item'>
+			<span>Sigma</span>
+		</div>
 		<div className='chat-list-item'>
 			<span>Kawkab</span>
 		</div>
