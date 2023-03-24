61afaa96ffe626a660a29d7f53a4dd21c8d14904
sigma-hub <houssna555@gmail.com>
2023-03-24 06:55:50 +0000

    working on settings boxes

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import enable2fa from './../assets/svg/enable2fa.svg'
+import nickname from './../assets/svg/nickname.svg'
+import editpic from './../assets/svg/editpic.svg'
 
 function Settings() {
   return (
 	<div className='settings-container'>
 		<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
-			<h1>Edit My Profil Picture</h1>
+			<img src={editpic} alt="" />
 		</div>
 		<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
-			<h1>Edit My Nickname</h1>
+			<img src={nickname} alt="" />
 		</div>
 		<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
-			<h1>Enable 2FA</h1>
+			<img src={enable2fa} alt="" />
 		</div>
 	</div>
   );
