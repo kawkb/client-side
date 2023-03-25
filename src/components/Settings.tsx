9e101391d434b1d343b222557cec8b5b73a7a938
sigma-hub <houssna555@gmail.com>
2023-03-25 03:36:07 +0000

    added toggle button flower still need to work on position more look good tho

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,33 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import enable2fa from './../assets/svg/enable2fa.svg'
 import nickname from './../assets/svg/nickname.svg'
 import editpic from './../assets/svg/editpic.svg'
 import heart from './../assets/svg/heart.svg'
+import greenflower from './../assets/svg/greenflower.svg'
+import redflower from './../assets/svg/redflower.svg'
 import ImgUpload from './ImgUpload'
 import ImgButton from './ImgButton'
+import ToggleButton from './ToggleButton'
+import CustomToggleButton from './CustomToggle'
 
 function Settings() {
+	// const [enable2FA, setEnable2FA] = useState(false);
+	
 	const handleNewNickname = () => {
 		console.log('Change Nickname!');
 	};
-  return (
-	<div className='settings-container'>
-		<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
-			<img className="svg-text" src={editpic} alt="" />
-			<ImgUpload />
-		</div>
-		<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
-			<img className="svg-text svg-text-margin" src={nickname} alt="" />
-			<div className='nickname-input'>
-				<input type="text" placeholder='New Nickname' />
-				<ImgButton src={heart} alt="confirm" onClick={handleNewNickname} />
+
+	// const handleToggle = (isToggled: boolean) => {
+	// 	setEnable2FA(isToggled);
+	// };
+
+	const handleToggle = (isToggled: boolean) => {
+		console.log(`Toggle button is ${isToggled ? 'on' : 'off'}`);
+	};
+
+	return (
+		<div className='settings-container'>
+			<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
+				<img className="svg-text" src={editpic} alt="" />
+				<ImgUpload />
+			</div>
+			<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
+				<img className="svg-text svg-text-margin" src={nickname} alt="" />
+				<div className='nickname-input'>
+					<input type="text" placeholder='New Nickname' />
+					<ImgButton src={heart} alt="confirm" onClick={handleNewNickname} />
+				</div>
+			</div>
+			<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
+				<img className="svg-text" src={enable2fa} alt="" />
+				<div className='toggle-container'>
+					<CustomToggleButton 
+						onToggle={handleToggle} 
+						untoggledImage={redflower}
+						toggledImage={greenflower}
+					/>
+				</div>
 			</div>
 		</div>
-		<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
-			<img className="svg-text" src={enable2fa} alt="" />
-		</div>
-	</div>
-  );
+	);
 }
 
 export default Settings
