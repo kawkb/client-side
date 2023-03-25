c7e2c798aa469721ed0f7442c201809bf7dce86b
sigma-hub <houssna555@gmail.com>
2023-03-25 00:59:26 +0000

    added image uploader component and button for profil pic edit

---
+++ src/components/ImgUpload.tsx
@@ -0,0 +1,29 @@
+import React, {useRef} from 'react'
+
+function ImgUpload() {
+	
+	const fileInputRef = useRef<HTMLInputElement>(null);
+
+	const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const file = event.target.files && event.target.files[0];
+		if (file) {
+			// Do something with the selected file
+			console.log(file);
+		}
+	};
+
+	const handleCLick = () => {
+		if (fileInputRef.current) {
+			fileInputRef.current.click();
+		}
+	};
+	
+	return (
+		<div className='retro-button-container'>
+			<button className='retro-button' onClick={handleCLick}>Click here</button>
+			<input type="file" accept="image/*" ref={fileInputRef} style={{display: 'none'}} onChange={handleFileInputChange} />
+		</div>
+  	);
+}
+
+export default ImgUpload
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import enable2fa from './../assets/svg/enable2fa.svg'
 import nickname from './../assets/svg/nickname.svg'
 import editpic from './../assets/svg/editpic.svg'
+import ImgUpload from './ImgUpload'
 
 function Settings() {
   return (
 	<div className='settings-container'>
 		<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
 			<img className="svg-text" src={editpic} alt="" />
+			<ImgUpload />
 		</div>
 		<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
 			<img className="svg-text svg-text-margin" src={nickname} alt="" />
+			
 		</div>
 		<div className='copy-book-background retro-border-box trans-pink-box setting-box'>
 			<img className="svg-text" src={enable2fa} alt="" />
