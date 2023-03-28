dd4a1bd9a5faa328cedb95e3b51fd58e229eb5c6
sigma-hub <houssna555@gmail.com>
2023-03-28 03:19:55 +0000

    progress on speech bubble message

--- src/components/atoms/ChatMsg.tsx
+++ src/components/atoms/ChatMsg.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import smiley from '../../assets/img/smiley.png'
+import smily3 from '../../assets/svg/smily3.svg'
 
 function ChatMsg() {
   return (
 	<div className='chat-msg owner'>
 		<div className='chat-msg-info'>
-			<img src={smiley} alt=''/>
+			<img src={smily3} alt=''/>
 			{/* <span>just now</span> */}
 		</div>
-		<div className='chat-msg-content'>
-			<p>
-Soju special stater kit ✔ already won the game ✔ This lobby’s playing for second ✔ This is my last loss ✔ I win out from here ✔ My board is too lit ✔ HP is fake ✔ I’m about to spike hard ✔ That’s a fake loss ✔ 20hp? That’s 3 lives ✔ This game is over ✔ We win out ✔ We're actually scaling ✔ last loss ✔ it's a fast 9 ✔ going eif</p>
+		{/* <div className='chat-msg-content'> */}
+		<div className='message-blurb-container'>
+			<p className='message-blurb'>hello world!</p>
+			{/* <p className='message-blurb'>
+Soju special stater kit ✔ already won the game ✔ This lobby’s playing for second ✔ This is my last loss ✔ I win out from here ✔ My board is too lit ✔ HP is fake ✔ I’m about to spike hard ✔ That’s a fake loss ✔ 20hp? That’s 3 lives ✔ This game is over ✔ We win out ✔ We're actually scaling ✔ last loss ✔ it's a fast 9 ✔ going eif</p> */}
 		</div>
 	</div>
   )
