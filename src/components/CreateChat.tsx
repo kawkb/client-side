87b554a6d33e86220c06cbe19b471ced3e86efe7
sigma-hub <houssna555@gmail.com>
2023-03-28 08:07:21 +0000

    added unclick to other new room choices when one is clicked plus new hover and color

--- src/components/CreateChat.tsx
+++ src/components/CreateChat.tsx
@@ -1,22 +1,32 @@
 import React from 'react'
+import { useState } from 'react';
 import ClassButton from './ClassButton'
 
 function CreateChat({onClose}: {onClose: () => void}) {
 
-	const handleCreatePublic = () => {}
-	const handleCreatePrivate = () => {}
-	const handleCreateProtected = () => {}
+	const [publicBtnState, setPublicBtnState] = useState(false);
+	const [privateBtnState, setPrivateBtnState] = useState(false);
+	const [protectedBtnState, setProtectedBtnState] = useState(false);
+
+	const handleCreatePublic = () => { const newBtnState = !publicBtnState; setPublicBtnState(newBtnState); if (newBtnState) {setPrivateBtnState(false); setProtectedBtnState(false);}}
+	const handleCreatePrivate = () => { const newBtnState = !privateBtnState; setPrivateBtnState(newBtnState); if (newBtnState) {setPublicBtnState(false); setProtectedBtnState(false);}}
+	const handleCreateProtected = () => { const newBtnState = !protectedBtnState; setProtectedBtnState(newBtnState); if (newBtnState) {setPrivateBtnState(false); setPublicBtnState(false);}}
 
 	const handleCreateNewRoom = () => {}
+
+	const publicClasses = "new-room-option-btn new-room-option-first-btn " + (publicBtnState? "green-bg" : "coral-bg"); 
+	const privateClasses = "new-room-option-btn " + (privateBtnState? "green-bg" : "coral-bg"); 
+	const protectedClasses = "new-room-option-btn " + (protectedBtnState? "green-bg" : "coral-bg"); 
+
 
   return (
 	<div className='create-room-popup copy-book-background retro-border-box trans-orange-box'>
 		<h1 className='create-room-title'>Add New Chat Room!</h1>
 		<input className="popup-input" type="text" placeholder='Room Name:'/>
 		<div className='create-chat-options'>
-			<ClassButton name="Public" classes="new-room-option-btn new-room-option-first-btn" onClick={handleCreatePublic}/>
-			<ClassButton name="Private" classes="new-room-option-btn" onClick={handleCreatePrivate}/>
-			<ClassButton name="Protected" classes="new-room-option-btn" onClick={handleCreateProtected}/>
+			<ClassButton name="Public" classes={publicClasses} onClick={handleCreatePublic}/>
+			<ClassButton name="Private" classes={privateClasses} onClick={handleCreatePrivate}/>
+			<ClassButton name="Protected" classes={protectedClasses} onClick={handleCreateProtected}/>
 		</div>
 		<input className="popup-input" type="password" placeholder='Password:'/>
 		<div className='create-button-container'>
