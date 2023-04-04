8e01191b6ba208bcb8def179736d13676f1e46a2
sigma-hub <houssna555@gmail.com>
2023-04-04 02:22:55 +0000

    added json server

--- src/components/ImgButton.tsx
+++ src/components/ImgButton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ImgButton({ src, alt, classes, onClick }: { src: string, alt: string, classes: string, onClick: () => void }) {
   return (
 	<button className={classes} onClick={onClick}>
-		<img src={src} alt={alt} style={{cursor: 'pointer', maxWidth: '100%'}} 
+		<img src={src} alt={alt} style={{cursor: 'pointer', maxWidth: '100%', maxHeight: '100%'}} 
 		onMouseOver={(e) => { e.currentTarget.style.transform = 'scale(1.1)'; }}
         onMouseOut={(e) => { e.currentTarget.style.transform = 'scale(1)'; }}/>
 	</button>
