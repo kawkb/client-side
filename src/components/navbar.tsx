76b72334b4375035deb037f6eb99f38b323af310
kawkb <dfi3lp@gmail.com>
2023-04-10 20:23:20 +0000

    Menu in process

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import logo from '../assets/svg/logo.svg'
-import { Link } from 'react-router-dom';
+import React from "react";
+import logo from "../assets/svg/logo.svg";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <div className='nav-container'>
-            <nav>
-                <Link to="/menu" className='nav-links'>Menu</Link>
-                <Link to='/' className="logo">
-                    <img src={logo} alt="logo" />
-                </Link>
-                <Link to="/Bell" className='nav-links'>Bell</Link>
-            </nav>
-        </div>
-    )
+  const [menu, setMenu] = React.useState({ name: "Menu", isOpen: false });
+
+  const nav = useNavigate();
+  function toggleMenu() {
+    if (menu.isOpen) {
+      setMenu({ name: "Close", isOpen: true });
+      nav(-1);
+    } else {
+      setMenu({ name: "Close", isOpen: true });
+    }
+  }
+  return (
+    <div className="nav-container">
+      <nav>
+        <Link to="/menu" className="nav-links" onClick={() => toggleMenu()}>
+          {" "}
+          {menu.name}
+        </Link>
+        <Link to="/" className="logo">
+          <img src={logo} alt="logo" />
+        </Link>
+        <Link to="/Bell" className="nav-links">
+          Bell
+        </Link>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
