0441d0b5fba4911aef19980d474bcca8f6782cb7
moerradi <moer@odoo.com>
2023-04-11 02:30:02 +0000

    [ADD] "connected auth and tried protected route on profil"

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,15 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 // import Cookies from 'js-cookie';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
+
+// api.interceptors.request.use(
+//     (config) => {
+//         // if we get a 401 force logout
+// if (config.
+//         // Cookies.remove('token');
 
 export default api;
